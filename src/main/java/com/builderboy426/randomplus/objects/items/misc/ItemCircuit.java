5297ab24185d125e91a55ede5883f7175e5c6f63
Builderboy426 <33360787+Builderboy426@users.noreply.github.com>
2019-04-08 22:10:19 -0400

    More Changes

--- src/main/java/com/builderboy426/randomplus/objects/items/misc/ItemCircuit.java
+++ src/main/java/com/builderboy426/randomplus/objects/items/misc/ItemCircuit.java
@@ -1,5 +1,25 @@
 package com.builderboy426.randomplus.objects.items.misc;
 
-public class ItemCircuit {
+import com.builderboy426.randomplus.objects.items.ItemBase;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.world.World;
+
+public class ItemCircuit extends ItemBase {
+
+	public ItemCircuit(String name) {
+		super(name, 16);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		world.playSound(player, player.getPosition(), new SoundEvent(new ResourceLocation("ui.button.click")), SoundCategory.MASTER, 0.5F, 0.0F);
+		return null;
+	}
 }
