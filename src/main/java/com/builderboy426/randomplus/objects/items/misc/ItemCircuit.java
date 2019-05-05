0b610b52dd0f77906b3fd0c1bc01a82b2e6c22b6
Builderboy426 <33360787+Builderboy426@users.noreply.github.com>
2019-05-05 17:40:06 -0400

    Circuit Fixed

--- src/main/java/com/builderboy426/randomplus/objects/items/misc/ItemCircuit.java
+++ src/main/java/com/builderboy426/randomplus/objects/items/misc/ItemCircuit.java
@@ -5,6 +5,7 @@ import com.builderboy426.randomplus.objects.items.ItemBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
@@ -19,7 +20,8 @@ public class ItemCircuit extends ItemBase {
 	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		ItemStack item = player.getHeldItem(hand);
 		world.playSound(player, player.getPosition(), new SoundEvent(new ResourceLocation("ui.button.click")), SoundCategory.MASTER, 0.5F, 0.0F);
-		return null;
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, item);
 	}
 }
